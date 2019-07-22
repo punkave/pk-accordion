5ab56e3f37d3ddb494afa0430e114df339fdb0ee
mtthwmnc <matthew.mance@gmail.com>
2019-07-22 15:26:42 -0400

    Adjust schema and markup. Add base styles

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ module.exports = {
     modules: ['pk-accordion-section-widgets', 'pk-accordion-widgets'],
     directory: 'lib/modules'
   },
-  directory: 'lib/modules'
+  afterConstruct: function (self) {
+    self.pushAsset('script', 'always', {
+      when: 'always'
+    });
+    self.pushAsset('stylesheet', 'always', {
+      when: 'always'
+    });
+  }
 };
